7ce016c001cd47903d82d6b1258566a2427c7e03
NazalPrastya <nazalprastya@gmail.com>
2023-07-14 20:50:46 +0700

    nazal

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '36px',
+    },
+    extend: {
+      colors: {
+        primary: '#28d07e',
+        secondary: '#4e764E',
+        background: '#161b21',
+        background2: '#1f252d',
+      },
+    },
+  },
+  plugins: [],
+};
