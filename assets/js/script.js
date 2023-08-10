b99fa3c62bd1dd174ebc2eb7b7569582b75fad26
NazalPrastya <nazalprastya@gmail.com>
2023-08-10 16:12:03 +0700

    nazal

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,30 +11,30 @@ window.onscroll = function () {
 };
 
 // Send Email
-document.getElementById('emailForm').addEventListener('submit', function (event) {
-  event.preventDefault(); // Menghentikan pengiriman form default
+// document.getElementById('emailForm').addEventListener('submit', function (event) {
+//   event.preventDefault(); // Menghentikan pengiriman form default
 
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  var message = document.getElementById('message').value;
+//   var name = document.getElementById('name').value;
+//   var email = document.getElementById('email').value;
+//   var message = document.getElementById('message').value;
 
-  var body = 'Nama: ' + name + '\nEmail: ' + email + '\nPesan:\n' + message;
+//   var body = 'Nama: ' + name + '\nEmail: ' + email + '\nPesan:\n' + message;
 
-  Email.send({
-    SecureToken: '11c7a92f-674c-42e9-840c-67d477d439a4',
-    To: 'nazalprastya@gmail.com',
-    From: email,
-    Subject: 'Pesan dari Form Kirim Email',
-    Body: body,
-  }).then(function (message) {
-    if (message === 'OK') {
-      alert('Email berhasil dikirim!');
-      document.getElementById('emailForm').reset();
-    } else {
-      alert('Maaf, terjadi kesalahan saat mengirim email.');
-    }
-  });
-});
+//   Email.send({
+//     SecureToken: '11c7a92f-674c-42e9-840c-67d477d439a4',
+//     To: 'nazalprastya@gmail.com',
+//     From: email,
+//     Subject: 'Pesan dari Form Kirim Email',
+//     Body: body,
+//   }).then(function (message) {
+//     if (message === 'OK') {
+//       alert('Email berhasil dikirim!');
+//       document.getElementById('emailForm').reset();
+//     } else {
+//       alert('Maaf, terjadi kesalahan saat mengirim email.');
+//     }
+//   });
+// });
 
 window.onload = function () {
   var downloadBtn = document.getElementById('downloadBtn');
