7438489ef7abadae9078f4ddbefe442844113f79
NazalPrastya <nazalprastya@gmail.com>
2025-04-11 18:33:14 +0700

    remove py experience

--- src/sections/Welcome/experience.tsx
+++ src/sections/Welcome/experience.tsx
@@ -3,7 +3,7 @@ import { Sparkles } from "lucide-react";
 
 export default function Experience() {
   return (
-    <div className="w-full container py-12">
+    <div className="w-full container">
       <div className="flex flex-col md:flex-row gap-8">
         {/* Left Column */}
         <div className="w-full md:w-1/3 space-y-4">
