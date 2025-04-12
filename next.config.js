f717cd3c3da24090b525399aa81f69d66602c01d
NazalPrastya <nazalprastya@gmail.com>
2025-04-12 12:22:14 +0700

    change env example

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+    transpilePackages: ['next-auth'],
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
