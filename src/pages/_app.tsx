389d933216be6e580d8c29bf7cf3c12ae7a14211
NazalPrastya <nazalprastya@gmail.com>
2025-04-12 12:20:53 +0700

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+import { Geist } from "next/font/google";
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+
+const geist = Geist({
+  subsets: ["latin"],
+});
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <div className={geist.className}>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
